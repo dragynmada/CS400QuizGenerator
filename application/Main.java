0a59f4875ee397b8dfd624b8be1ea3a1b7285f80
Grant <grantmbb@live.com>
2019-04-22 18:59:49 -0500

    ListView

--- application/Main.java
+++ application/Main.java
@@ -10,6 +10,7 @@ import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -27,28 +28,37 @@ public class Main extends Application {
 			Button importBooks = new Button("Import JSON");
 			Button export = new Button("Export");
 			
+			// Add ArrayList for Questions
 			
-			questionList.put("Hungry", null);
-			questionList.put("Box", null);
+			questionList.put("Hungry", new ArrayList<Question>());
+			questionList.put("Box", new ArrayList<Question>());
 			
 			
 			ObservableList<String> topicList = FXCollections.observableArrayList(questionList.keySet());
 			
 			Collections.sort(topicList);
 		
-			ComboBox topics = new ComboBox();
-			
+			ComboBox<String> topics = new ComboBox<String>();
 			topics.setItems(topicList);
+			topics.getSelectionModel().selectFirst();
+			ArrayList<Question> topicQuestions = questionList.get(topics.getValue());
+			ObservableList<String> questionTitles = FXCollections.observableArrayList();
+			
+			
+			
+			for(Question q : topicQuestions) {
+				questionTitles.add(q.getQuestionTitle());
+			}
+			
+			ListView<String> list = new ListView<String>(questionTitles);
 			
 			hungryBox1.setStyle("-fx-border-color:black;");
 			hungryBox1.setStyle("-fx-border-radius:5;");
 			takeQuiz.setStyle("-fx-font-size:60px;");
 			
-			
-			
 			hungryBox1.getChildren().addAll(topics, takeQuiz);
 			hungryBox2.getChildren().addAll(addNewQuestion, importBooks, export);
-			vungryBox.getChildren().addAll(hungryBox1, hungryBox2);
+			vungryBox.getChildren().addAll(hungryBox1, hungryBox2, list);
 			Scene scene = new Scene(vungryBox, 400, 400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
--- application/Question.java
+++ application/Question.java
@@ -1,11 +1,31 @@
 package application;
 
+import java.util.ArrayList;
+import javafx.scene.image.ImageView;
+
 public class Question {
-	
-	
-	public Question() {
-		
-	}
-	
-	
+  String questionTitle;
+  ArrayList<String> options;
+  int correctIndex;
+  ImageView image;
+
+  public Question(String questionTitle, ArrayList<String> options, int correctIndex,
+      ImageView image) {
+    this.questionTitle = questionTitle;
+    this.options = options;
+    this.correctIndex = correctIndex;
+    this.image = image;
+  }
+
+  public int getCorrect() {
+    return this.correctIndex;
+  }
+
+  public ArrayList<String> getOptions() {
+    return options;
+  }
+
+  public String getQuestionTitle() {
+    return this.questionTitle;
+  }
 }
